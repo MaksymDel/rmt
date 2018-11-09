dd5e9c7225f32c3864fd9716b6b5358d91e247fc
maksym.del <max.del.edu@gmail.com>
2018-11-09 03:14:30 +0200

    Refactor mapping layer

--- rmt/models/semantic_spaces_mapper.py
+++ rmt/models/semantic_spaces_mapper.py
@@ -11,8 +11,10 @@ from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
 from allennlp.modules import TimeDistributed, Seq2SeqEncoder
+from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
 from torch.nn.functional import mse_loss
-from allennlp.nn.util import get_mask_from_sequence_lengths
+from allennlp.nn.util import get_mask_from_sequence_lengths, masked_mean
+
 
 # TODO: better naming for everything in the codebase
 # encoded_target -> target_vectors
@@ -38,20 +40,24 @@ class SemanticSpacesMapper(Model):
 
     """
     def __init__(self, vocab: Vocabulary,
+                 encoder: Seq2SeqEncoder,
                  mapping_layer: Seq2SeqEncoder,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super().__init__(vocab, regularizer)
         initializer(self)
 
+        assert encoder.get_output_dim() == mapping_layer.get_input_dim()
+
+        self._encoder = encoder # can be bypassed (use BypassEncoder) since input is already encoded with e.g. BERT
         self._mapping_layer = mapping_layer
 
     @overrides
     def forward(self,  # type: ignore
                 encoded_src: torch.Tensor,
                 src_strings: List[str],
-                encoded_tgt: torch.Tensor=None,
-                tgt_strings: List[str]=None) -> Dict[str, torch.Tensor]:
+                encoded_tgt: torch.Tensor = None,
+                tgt_strings: List[str] = None) -> Dict[str, torch.Tensor]:
         # pylint: disable=arguments-differ
         """
         Parameters
@@ -64,11 +70,55 @@ class SemanticSpacesMapper(Model):
         src_mask = self._get_mask_from_token_strings(src_strings)
         tgt_mask = self._get_mask_from_token_strings(tgt_strings) 
 
-        estimated_encoded_tgt = self._mapping_layer(encoded_src, src_mask, encoded_tgt, tgt_mask)
-        
-        assert encoded_tgt.size() == estimated_encoded_tgt.size() 
+        # prepare source vectors for decoding
+        state = self._init_encoded_state(encoded_src, src_mask)
+
+        # generate target embeddings
+        estimated_encoded_tgt = self._mapping_layer(state, encoded_tgt)
+
+        # compute MSE loss wrt golden target embeddings
+        assert encoded_tgt.size() == estimated_encoded_tgt.size()
         loss = mse_loss(estimated_encoded_tgt, encoded_tgt)
         return {"loss": loss}
+
+    def _init_encoded_state(self, embedded_input: torch.Tensor, source_mask: torch.LongTensor) -> Dict[str, torch.Tensor]:
+        """
+        Initialize the encoded state to be passed to the first decoding time step.
+        """
+        # shape: (batch_size, max_input_sequence_length, encoder_input_dim)
+        batch_size, _, _ = embedded_input.size()
+
+        # shape: (batch_size, max_input_sequence_length, encoder_output_dim)
+        encoder_outputs = self._encoder(embedded_input, source_mask)
+
+        # Initialize the decoder hidden state with the final output of the encoder.
+        # shape: (batch_size, decoder_output_dim)
+        decoder_hidden = self._get_summary_of_encoder_outputs(encoder_outputs, source_mask)
+        
+        # shape: (batch_size, decoder_output_dim)
+        decoder_context = encoder_outputs.new_zeros(batch_size, self._mapping_layer.get_output_dim())
+
+        state = {
+                "source_mask": source_mask,
+                "encoder_outputs": encoder_outputs,
+                "decoder_hidden": decoder_hidden,
+                "decoder_context": decoder_context,
+                "encoded_bos_symbol": embedded_input[:, 0, :]
+        }
+
+        return state
+
+    def _get_summary_of_encoder_outputs(self, encoder_outputs, source_mask):
+        # This returns last final encoder output in case of RNN encoders,
+        # and mean of the outputs in case of other encoders
+        if type(self._encoder) == PytorchSeq2SeqWrapper:
+            summary = util.get_final_encoder_states(
+                                                encoder_outputs,
+                                                source_mask,
+                                                self._encoder.is_bidirectional())
+        else:
+            summary = masked_mean(encoder_outputs, source_mask.unsqueeze(-1), dim=1, keepdim=False)
+        return summary
 
     @staticmethod
     def _get_mask_from_token_strings(token_strings):
--- rmt/modules/att_rnn_decoder.py
+++ rmt/modules/att_rnn_decoder.py
@@ -19,7 +19,8 @@ from allennlp.nn import util
 from allennlp.nn.beam_search import BeamSearch
 
 
-@Seq2SeqEncoder.register("att_rnn2rnn")
+# TODO: make abstract class Seq2SeqDecoder and make this module inherit it
+@Seq2SeqEncoder.register("att_rnn_decoder")
 class AttentionalRnnDecoder(Seq2SeqEncoder):
     """
     Maps source embeddings to target embeddings ommiting softmax layer. We do not need softmax layer
@@ -28,8 +29,8 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
 
     Parameters
     ----------
-    encoder : ``Seq2SeqEncoder``, required
-        The encoder of the "encoder/decoder" model
+    input_dim : int, required
+        Feature dimention of inputs
     max_decoding_steps : ``int``
         Maximum length of decoded sequences.
     target_embedding_dim : ``int``, optional (default = source_embedding_dim)
@@ -53,23 +54,17 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
     """
 
     def __init__(self,
-                 encoder: Seq2SeqEncoder,
+                 input_dim: int,
+                 decoder_rnn_output_dim: int,
+                 output_projection_dim: int,
                  max_decoding_steps: int,
                  attention: Attention = None,
                  attention_function: SimilarityFunction = None,
-                 target_embedding_dim: int = None,
                  scheduled_sampling_ratio: float = 0.) -> None:
         super(AttentionalRnnDecoder, self).__init__()
         self._scheduled_sampling_ratio = scheduled_sampling_ratio
-
-        # We need the start symbol to provide as the input at the first timestep of decoding, and
-        # end symbol as a way to indicate the end of the decoded sequence.
-        self._encoed_start_symbol = None
-
-        # Encodes the sequence of source embeddings into a sequence of hidden states.
-        self._encoder = encoder
-
-        target_embedding_dim = target_embedding_dim or encoder.get_input_dim()
+        self._input_dim = input_dim
+        self._output_dim = output_projection_dim
 
         # Attention mechanism applied to the encoder output for each step.
         if attention:
@@ -82,44 +77,31 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
         else:
             self._attention = None
 
-        # Decoder output dim needs to be the same as the encoder output dim since we initialize the
-        # hidden state of the decoder with the final hidden state of the encoder.
-        self._encoder_output_dim = self._encoder.get_output_dim()
-        self._decoder_output_dim = self._encoder_output_dim
-
         if self._attention:
             # If using attention, a weighted average over encoder outputs will be concatenated
             # to the previous target embedding to form the input to the decoder at each
             # time step.
-            self._decoder_input_dim = self._decoder_output_dim + target_embedding_dim
+            decoder_rnn_input_dim = input_dim + output_projection_dim
         else:
             # Otherwise, the input to the decoder is just the previous target embedding.
-            self._decoder_input_dim = target_embedding_dim
+            decoder_rnn_input_dim = output_projection_dim
 
         # We'll use an LSTM cell as the recurrent cell that produces a hidden state
         # for the decoder at each time step.
         # TODO (pradeep): Do not hardcode decoder cell type.
-        self._decoder_cell = LSTMCell(self._decoder_input_dim, self._decoder_output_dim)
+        self._decoder_cell = LSTMCell(decoder_rnn_input_dim, decoder_rnn_output_dim)
 
         # We project the hidden state from the decoder into the output vocabulary space
         # in order to get log probabilities of each target token, at each time step.
-        self._output_projection_layer = Linear(self._decoder_output_dim, target_embedding_dim)
+        self._output_projection_layer = Linear(decoder_rnn_output_dim, output_projection_dim)
 
         # At prediction time, we can use a beam search to find the most likely sequence of target tokens.
         # If the beam_size parameter is not given, we'll just use a greedy search (equivalent to beam_size = 1).
         self._max_decoding_steps = max_decoding_steps
 
-        # Encoded BOS symbol to be set from the outside
-        self._encoded_bos_symbol = None
-    
     @overrides
-    def forward(self,  # type: ignore
-                encoded_src: Dict[str, torch.LongTensor],
-                src_mask: torch.LongTensor,
-                encoded_tgt: Dict[str, torch.LongTensor]=None,
-                tgt_mask: torch.LongTensor=None) -> Dict[str, torch.Tensor]:
-
-        # pylint: disable=arguments-differ
+    def forward(self, state: Dict[str, torch.Tensor],
+                      encoded_tgt: torch.FloatTensor = None) -> Dict[str, torch.Tensor]:
         """
         Make foward pass with decoder logic for producing the entire target sequence of embeddings.
 
@@ -130,63 +112,11 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
         -------
         Dict[str, torch.Tensor]
         """
-        if self._encoded_bos_symbol is None:
-            # we set this to bos ymbol from src  since it is always available and the same as 
-            # the target's one for many encoders with shared vocabulary like BERT form GoogleAI
-            self._set_encoded_bos_symbol(encoded_src[:, 0, :])
-
-        state = self._init_encoded_state(encoded_src, src_mask)
-        return self._forward_loop(state, encoded_tgt, tgt_mask)
-
-    def _set_encoded_bos_symbol(self, bos_symbol_embedding: torch.FloatTensor):
-        """ Input shape should be (batch_size, embedding_dim) """
-        self._encoded_bos_symbol = bos_symbol_embedding
-
-    def _init_encoded_state(self, embedded_input: torch.Tensor, source_mask: torch.LongTensor) -> Dict[str, torch.Tensor]:
-        """
-        Initialize the encoded state to be passed to the first decoding time step.
-        """
-        # shape: (batch_size, max_input_sequence_length, encoder_input_dim)
-        batch_size, _, _ = embedded_input.size()
-
-        # shape: (batch_size, max_input_sequence_length, encoder_output_dim)
-        encoder_outputs = self._encoder(embedded_input, source_mask)
-
-        # shape: (batch_size, encoder_output_dim)
-        final_encoder_output = util.get_final_encoder_states(
-                encoder_outputs,
-                source_mask,
-                self._encoder.is_bidirectional())
-
-        # Initialize the decoder hidden state with the final output of the encoder.
-        # shape: (batch_size, decoder_output_dim)
-        decoder_hidden = final_encoder_output
-
-        # shape: (batch_size, decoder_output_dim)
-        decoder_context = encoder_outputs.new_zeros(batch_size, self._decoder_output_dim)
-
-        state = {
-                "source_mask": source_mask,
-                "encoder_outputs": encoder_outputs,
-                "decoder_hidden": decoder_hidden,
-                "decoder_context": decoder_context
-        }
-
-        return state
-
-    def _forward_loop(self,
-                      state: Dict[str, torch.Tensor],
-                      encoded_tgt: torch.FloatTensor=None,
-                      tgt_mask: torch.LongTensor=None) -> Dict[str, torch.Tensor]:
-        """Make forward pass during training or do greedy search during prediction."""
-        # shape: (batch_size, max_input_sequence_length)
-        source_mask = state["source_mask"]
-
-        batch_size = source_mask.size()[0]
-        encoded_tgt_dim = encoded_tgt.size()[2]
+        #batch_size = source_mask.size()[0]
+        #encoded_tgt_dim = encoded_tgt.size()[2]
 
         if encoded_tgt is not None:
-            _, target_sequence_length = tgt_mask.size()
+            _, target_sequence_length, _ = encoded_tgt.size()
 
             # The last input from the target is either padding or the end symbol.
             # Either way, we don't have to process it.
@@ -197,8 +127,7 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
         # Initialize target predictions with the encoded BOS symbol 
         # to feed to the decoder when targets are not available.
         # shape: (batch_size, encoded_tgt_dim)
-        # make sure you set self._encoded_bos_symbol in advance) 
-        last_predictions_encoded = self._encoded_bos_symbol
+        last_predictions_encoded = state["encoded_bos_symbol"]
 
         # Append encoded BOS symbol to the output predictions becasuse we want the returnet 
         # sequence of embeddeddings to be in the same format as the input sequence
@@ -295,3 +224,11 @@ class AttentionalRnnDecoder(Seq2SeqEncoder):
         attended_input = util.weighted_sum(encoder_outputs, input_weights)
 
         return attended_input
+            
+    @overrides
+    def get_input_dim(self):
+        return self._input_dim
+
+    @overrides
+    def get_output_dim(self):
+        return self._output_dim
